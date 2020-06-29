e9cee313829267803408681af495a9df23ff569d
Wojtek_linus <wojtekbabij@wp.pl>
2020-06-29 23:48:39 +0200

    Napisanie kodu w klasie Class i Test

--- src/main/java/introduction_to_unit_testing/Test.java
+++ src/main/java/introduction_to_unit_testing/Test.java
@@ -9,7 +9,7 @@ public class Test {
         Calc calc = new Calc();
         int result = calc.sum(2,2);
         if(result != 4) {
-            throw new IllegalStateException("Wrong result, 2+2 is not");
+            throw new IllegalStateException("Wrong result, 2+2 is not 4");
         } else {
             System.out.println("Everything is ok");
         }
