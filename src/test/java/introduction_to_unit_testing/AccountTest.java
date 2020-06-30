fc45c155292a62ba04196d629efd6915573342ea
Wojtek_linus <wojtekbabij@wp.pl>
2020-06-30 17:34:48 +0200

    Podział testu na część given, when, then

--- src/test/java/introduction_to_unit_testing/AccountTest.java
+++ src/test/java/introduction_to_unit_testing/AccountTest.java
@@ -8,17 +8,27 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class AccountTest {
 
     @Test
-    public void myTest() {
+    public void thatNewAccountShouldBeActiveAfterCreation() {
+        //given + when
         Account newAccount = new Account();
+
+        //then
         assertFalse(newAccount.isActive(), "Check if account is not active"); //sprawdzenie czy prametr pokaże fase; sprawdzenie czy dla nowego konta prametr account jest false
     }
 
     @Test
-    public void myTest2() {
+    public void thatNewAccountShouldBeActiveAfterActivation() {
+        //given
         Account newAccount = new Account();
         assertFalse(newAccount.isActive(), "Check if account is not active");
+
+        //when
         newAccount.activate();
+
+        //then
         assertTrue(newAccount.isActive()); //sprawdzenie czy konto zostało aktywowane po wykonaniu metody activate()
     }
+
+
 
 }
